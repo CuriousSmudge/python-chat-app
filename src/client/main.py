4bf086e6ac141558574339c1b86d19b737c0d745
Knaughty <77908661+Kn4ughty@users.noreply.github.com>
2024-10-02 14:30:34 +1000

    basic demo of required librarys

--- src/client/main.py
+++ src/client/main.py
@@ -1,7 +1,43 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import QSize
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 # Only needed for access to command line arguments
 import sys
+
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        self.button_checked = True
+
+        self.button = QPushButton("Press Me!")
+        self.button.setCheckable(True)
+        self.button.setChecked(self.button_checked)
+        self.button.clicked.connect(self.the_button_was_toggled)
+
+        self.setMinimumSize(QSize(100, 100))
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(self.button)
+
+        self.times_pressed = 0
+
+    def the_button_was_toggled(self, checked):
+        self.button_checked = checked
+        self.times_pressed += 1
+        self.button.setText(f"I have been pressed {self.times_pressed} time/s\n You wont be able to press me after the 3rd click")
+
+        if self.times_pressed > 3:
+            self.button.setText("Cant click me again!!")
+            self.button.setEnabled(False)
+
+        print("checked?", self.button_checked)
+
+# This code from https://www.pythonguis.com/tutorials/pyqt6-creating-your-first-window/
 
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for your app.
@@ -9,7 +45,7 @@ import sys
 app = QApplication(sys.argv)
 
 # Create a Qt widget, which will be our window.
-window = QWidget()
+window = MainWindow()
 window.show()  # IMPORTANT!!!!! Windows are hidden by default.
 
 # Start the event loop.
--- src/server/main.py
+++ src/server/main.py
@@ -1,1 +1,24 @@
-import flask
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+@app.route("/user/profile-<userID>")
+def hello_world(userID):
+    return f"here is the data for {userID}: \n blah blah"
+
+@app.route("/user/create", methods=["POST"])
+def create_user():
+    data = request.json
+    print(data)
+    
+    if data is None or data == "":
+        return 'bad request!', 400
+
+    try:
+        username = data["username"]
+        password_hash = data["password_hash"]
+    except KeyError:
+        return f"missing field {KeyError}", 400
+
+    return str()
